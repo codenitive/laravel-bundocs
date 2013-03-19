d13e2f010460a2c8d910586d286f9f25bc006929
crynobone <crynobone@gmail.com>
2013-03-19 22:33:21 +0800

    Fixed bug when Orchestra Platform user is not logged-in.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- orchestra.php
+++ orchestra.php
@@ -3,8 +3,11 @@
 Event::listen('orchestra.done: backend', function()
 {
 	$menu = Orchestra\Core::menu();
+	$position = 'childof:settings';
 
-	$menu->add('bundocs', 'childof:settings')
+	Auth::guest() and $position = 'after:home';
+
+	$menu->add('bundocs', $position)
 		->title('View Documentation')
 		->link(handles('bundocs::orchestra'));
 });
